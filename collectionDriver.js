a41e17860f2e1f205f7ff159edc31eb7c463f352
Gregory Kielian <gregory.kielian@gmail.com>
2016-01-01 18:53:11 +0100

    add live data updates via mongodb + handlebars add data view, collection js, mod index.js

---
+++ collectionDriver.js
@@ -0,0 +1,57 @@
+var ObjectID = require('mongodb').ObjectID;
+
+
+CollectionDriver = function(db) {
+  this.db = db;
+};
+
+CollectionDriver.prototype.getCollection = function(collectionName, callback) {
+  this.db.collection(collectionName, function(error, the_collection) {
+    if( error ) callback(error);
+    else callback(null, the_collection);
+  });
+};
+
+CollectionDriver.prototype.findAll = function(collectionName, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error);
+      else {
+        the_collection.find().toArray(function(error, results) { //B
+          if( error ) callback(error);
+          else callback(null, results);
+        });
+      }
+    });
+};
+
+
+CollectionDriver.prototype.get = function(collectionName, id, callback) { //A
+  this.getCollection(collectionName, function(error, the_collection) {
+    if (error) callback(error);
+    else {
+      var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$"); //B
+      if (!checkForHexRegExp.test(id)) callback({error: "invalid id"});
+      else the_collection.findOne({'_id':ObjectID(id)}, function(error,doc) { //C
+        if (error) callback(error);
+        else callback(null, doc);
+      });
+    }
+  });
+};
+
+//save new object
+CollectionDriver.prototype.save = function(collectionName, obj, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) {
+        callback(error);
+      } else {
+        obj.created_at = new Date(); //B
+        the_collection.insert(obj, function() { //C
+          callback(null, obj);
+        });
+      }
+    });
+};
+exports.CollectionDriver = CollectionDriver;
+
+
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var express = require('express');
+var body_parser = require('body-parser');
 var exp_hbs = require('express-handlebars');
 var path = require('path');
+
+MongoClient = require('mongodb').MongoClient;
+Server = require('mongodb').Server;
+CollectionDriver = require('./collectionDriver').CollectionDriver;
 
 var app = express();
 
@@ -10,13 +15,36 @@ var public_dir = './public/';
 
 //sets the port number
 app.set('port', process.env.PORT || 3000);
+
+
+///////////////
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017; 
+var collectionDriver;
+
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
+mongoClient.open(function(err, mongoClient) { //C
+  if (!mongoClient) {
+    console.error("Error! Exiting... Must start MongoDB first");
+    process.exit(1); //D
+  }
+  var db = mongoClient.db("MyDatabase");  //E
+  collectionDriver = new CollectionDriver(db); //F
+});
+
 app.set('views', path.join(__dirname,'views'));
 app.engine('handlebars',
            exp_hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+//impt that this goes beore app.use and after app.set
+app.use(body_parser.urlencoded({
+    extended: true
+}));
+app.use(body_parser.json());
+
 
 //we'll have static files in ./public folder
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Examples of how to serve dynamic and static views
@@ -44,6 +72,48 @@ app.get('/opened-at-timestamp', function (req, res) {
 app.get('/static-example', function (req, res) {
   res.sendFile(public_dir + 'static-example.html');
 });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/:collection', function(req, res) { //A
+  var params = req.params; //B
+  collectionDriver.findAll(req.params.collection, function(error, objs) { //C
+    if (error) { res.send(400, error); } //D
+    else { 
+      if (req.accepts('html')) { //E
+        res.render('data',{objects: objs, collection: req.params.collection}); //F
+      } else {
+        res.set('Content-Type','application/json'); //G
+        res.status(200).send(objs); //H
+      }
+    }
+  });
+});
+
+app.get('/:collection/:entity', function(req, res) { //I
+  var params = req.params;
+  var entity = params.entity;
+  var collection = params.collection;
+  if (entity) {
+    collectionDriver.get(collection, entity, function(error, objs) { //J
+      if (error) { res.status(400).send(error); }
+      else { res.status(200).send(objs); } //K
+    });
+  } else {
+    res.status(400).send({error: 'bad url', url: req.url});
+  }
+});
+
+app.post('/:collection', function(req, res) { //A
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.status(400).send(err); } 
+          else { res.status(201).send(docs); } //B
+     });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //page not found
